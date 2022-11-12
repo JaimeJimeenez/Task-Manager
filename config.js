eaa9992f92b003172589d7a4b9532aae32acd191
Jaime Jimenez <jaimejimenez99@hotmail.com>
2022-11-12 21:13:00 +0100

    start practice 4

---
+++ config.js
@@ -0,0 +1,14 @@
+"use strict"
+
+module.exports = {
+
+    mysqlConfig: {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "TasksManager"
+    },
+
+    port: 3000
+    
+};  
