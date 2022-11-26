4832d3d15d6280513f15b53a1d3c0401e9d1fb07
Jaime Jimenez <jaimejimenez99@hotmail.com>
2022-11-26 21:24:02 +0100

    started practice 5

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(bodyParser.urlencoded( { extended: false }));
+app.use(bodyParser.urlencoded( { extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Using morgan
